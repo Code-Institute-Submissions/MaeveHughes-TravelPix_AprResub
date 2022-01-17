9059e8d96c1c0e6864f43672e84741edbeb8bd34
MaeveHughes <maevehughes@gmail.com>
2022-01-17 19:19:07 +0000

    Removing the PostList class duplicate from the views py file

--- photolog/views.py
+++ photolog/views.py
@@ -8,18 +8,11 @@ class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
-    paginate_by = 10
+    paginate_by = 6
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
 from .forms import CommentForm
-
-
-class PostList(generic.ListView):
-    model = Post
-    queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "index.html"
-    paginate_by = 6
 
 
 class PostDetail(View):
