bda0d2f9a807e8c503c1ebfb5049990b3bbf9808
MaeveHughes <maevehughes@gmail.com>
2022-01-22 00:06:46 +0000

    deployment xommit

--- travelpix/settings.py
+++ travelpix/settings.py
@@ -166,15 +166,3 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#SMTP Configuration
-
-
-if "DEV" in os.environ:
-    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-    DEFAULT_FROM_EMAIL = "photopix@example.com"
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    DEAFULT_FROM_EMAIL ="photopix@example.com"
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
