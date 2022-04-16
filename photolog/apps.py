57207ed33c7c2e60f36f0558489f74d5ce70f0b8
MaeveHughes <maevehughes@gmail.com>
2022-04-16 15:52:04 +0000

    Updating any problems identitified in the console for the apps.py file

--- photolog/apps.py
+++ photolog/apps.py
@@ -1,8 +1,9 @@
+"""Config for apps"""
+
 from django.apps import AppConfig
 
 
 class PhotologConfig(AppConfig):
+    """Config for Photolog App"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'photolog'
-
-
