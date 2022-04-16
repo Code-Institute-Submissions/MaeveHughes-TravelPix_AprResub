f578272abd7b5da1eebc174be962fbc46da47874
MaeveHughes <maevehughes@gmail.com>
2022-04-16 13:38:23 +0000

    Updating any problems identified in the console for the photolog urls

--- photolog/urls.py
+++ photolog/urls.py
@@ -1,12 +1,19 @@
-from . import views
+"""URL's"""
+
 from django.urls import path
-from .views import AddPostView, UpdatePostView, DeletePostView, Account
+from . import views
+from .views import AddPostView
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('add_post/', AddPostView.as_view(), name='add_post'),
-    path('edit_post/<int:pk>', views.UpdatePostView.as_view(), name='edit_post'),
-    path('delete_post/<int:pk>', views.DeletePostView.as_view(), name='delete_post'),
+    path(
+        'add_post/', AddPostView.as_view(), name='add_post'
+    ),
+    path(
+        'edit_post/<int:pk>', views.UpdatePostView.as_view(), name='edit_post'
+    ),
+    path('delete_post/<int:pk>', views.DeletePostView.as_view(),
+         name='delete_post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
