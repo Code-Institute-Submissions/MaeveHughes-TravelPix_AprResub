2e1b9359bc04e9ac73eb869f3562dd424b724cf2
MaeveHughes <maevehughes@gmail.com>
2022-04-16 15:55:48 +0000

    Updating any problems identitified in the console for the admin.py file

--- photolog/admin.py
+++ photolog/admin.py
@@ -1,24 +1,31 @@
+"""Options for admins page"""
+
 from django.contrib import admin
-from .models import Post, Comment, Account, Friend
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, Comment, Account, Friend
+
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    """Admins post model features"""
     summernote_fields = ('content',)
     list_filter = ('status', 'created_on')
     list_display = ('title', 'author', 'status', 'created_on')
     search_fields = ['title', 'content', 'author']
 
+
 admin.site.register(Account)
 admin.site.register(Friend)
 
+
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    """Admins post model features"""
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
 
-    def approve_comments(self, request, queryset):
+    def approve_comments(self, queryset):
+        """Approve comments"""
         queryset.update(approved=True)
-
