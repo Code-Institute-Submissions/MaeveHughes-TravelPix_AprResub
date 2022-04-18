dbe655aaccdf7b09c8611948be1363eb2cab6425
MaeveHughes <maevehughes@gmail.com>
2022-04-18 19:21:51 +0000

    Updating the settings

--- travelpix/settings.py
+++ travelpix/settings.py
@@ -174,13 +174,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # SMTP Configuration
 
-
-if "DEV" in os.environ:
-    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-    DEFAULT_FROM_EMAIL = "photopix@example.com"
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'photopix@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    DEAFULT_FROM_EMAIL = "photopix@example.com"
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
     EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
